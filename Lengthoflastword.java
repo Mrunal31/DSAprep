df3997521aab7984e25475a01ef62c23fc706263
Mrunal Joshi <87536784+Mrunal31@users.noreply.github.com>
2022-07-11 10:41:58 +0530

    Create Lengthoflastword.java

---
+++ Lengthoflastword.java
@@ -0,0 +1,6 @@
+LENGTH OF LAST WORD(STRING TYPE QUESTION LEETCODE) LEVEL-EASY 
+TIME REQ = 03:50:71
+
+String[] arr = new String[s.length()];
+arr = s.split(" ");
+return arr[arr.length - 1].length();
